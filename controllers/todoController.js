c7a775bcecd100fa0f671562d4e34d11343eee3a
kelvkedyson <kelvinkedysonzacharia@gmail.com>
2020-05-07 22:26:02 +0300

    Minor changes

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,30 +1,30 @@
 //body-parser module
 var bodyParser = require('body-parser');
 
-var urlencodedParser = bodyParser.urlencoded({extended: false});
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 //todo list array data..
-var data = [{item: 'Make bed'},{item: 'Take shower'}, {item: 'Have breakfast'}];
+var data = [{ item: 'Make bed' }, { item: 'Take shower' }, { item: 'Have breakfast' }];
 
 module.exports = function(app) {
 
     //handling the routes
     app.get('/todo', (req, res) => {
-        res.render('todo', {todos : data});
+        res.render('todo', { todos: data });
     });
 
     //handling POST requests
     app.post('/todo', urlencodedParser, (req, res) => {
-		data.push(req.body);
-		res.json(data);
+        data.push(req.body);
+        res.json(data);
     });
 
     //handling DELETE requests
     app.delete('/todo/:item', (req, res) => {
-		data = data.filter(function(todo){
-			return todo.item.replace(/ /g, "-") !== req.params.item;
-		});
+        data = data.filter(function(todo) {
+            return todo.item.replace(/ /g, "-") !== req.params.item;
+        });
 
-		res.json(data);
+        res.json(data);
     });
 };
